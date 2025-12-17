164f1383bc59cd283e5d01c95dc06d45fa5e43eb
Naveed <23211a05k3@bvrit.ac.in>
2025-12-17 16:39:55 +0530

    Implement Accounts features in Accounts.java

--- src/Accounts.java
+++ src/Accounts.java
@@ -10,19 +10,53 @@ public class Accounts {
         this.scanner = scanner;
     }
 
-    public long openAccount(long accNumber, String fullName, String email, double balance, String securityPin) {
-        throw new UnsupportedOperationException("openAccount not implemented yet");
+    public long openAccount(long accountNumber, String fullName, String email, double balance, String securityPin) {
+        String openAccountQuery = "INSERT INTO accounts(account_number, full_name, email, balance, security_pin) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(openAccountQuery)) {
+            preparedStatement.setLong(1, accountNumber);
+            preparedStatement.setString(2, fullName);
+            preparedStatement.setString(3, email);
+            preparedStatement.setDouble(4, balance);
+            preparedStatement.setString(5, securityPin);
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) return accountNumber;
+            else throw new RuntimeException("Account Creation failed!!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public boolean accountExists(String email) {
-        throw new UnsupportedOperationException("accountExists not implemented yet");
+    public boolean accountExists(String userEmail) {
+        String existsQuery = "SELECT account_number FROM accounts WHERE email = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(existsQuery)) {
+            preparedStatement.setString(1, userEmail);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public long getAccountNumber(String email) {
-        throw new UnsupportedOperationException("getAccount not implemented yet");
+    public long getAccountNumber(String userEmail) {
+        String getAccNumQuery = "SELECT account_number FROM accounts WHERE email = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(getAccNumQuery)) {
+            preparedStatement.setString(1, userEmail);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) return resultSet.getLong("account_number");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        throw new RuntimeException("Account Number Doesn't Exist");
     }
 
     public long generateAccountNumber() {
-        throw new UnsupportedOperationException("generateAccountNumber not implemented yet");
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT account_number from Accounts ORDER BY account_number DESC LIMIT 1")
+        ) {
+            if (resultSet.next()) return resultSet.getLong("account_number") + 1;
+            else return 10001000;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
